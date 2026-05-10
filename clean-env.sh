070f10916be1c9fa4c6116a699c54c0d342b200d
João Pedro Toaldo <toaldojoaopedro@gmail.com>
2026-05-10 11:58:45 -0300

    (security): confirma rotação de credenciais

--- clean-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-export FILTER_BRANCH_SQUELCH_WARNING=1
-git filter-branch -f --tree-filter 'test -f .env.production && rm .env.production; true' -- --all
-git reflog expire --expire=now --all
-git gc --prune=now --aggressive
